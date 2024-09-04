8f3287cb3fcb083a9bfc513ee45d3b824c44da70
farah <farahashraf268f@gmail.com>
2024-09-04 18:36:32 +0300

    add logs to stripe webhook

--- src/initApp.js
+++ src/initApp.js
@@ -11,12 +11,17 @@ export const initApp = (app, express) => {
   //stripe calls this api 
   app.post('/webhook', express.raw({ type: 'application/json' }), asyncHandler(async (req, res) => {
     let event = req.body;
+    console.log('req.body', req.body)
     // Only verify the event if you have an endpoint secret defined.
     // Otherwise use the basic event deserialized with JSON.
     const endpointSecret = 'whsec_CHm50gJSDefBrJFKf6k7rLSQpDNBFK4X';
-
+    console.log('endpointSecret', endpointSecret)
+    console.log('req.headers', req.headers)
+    console.log('req.headers[stripe-signature]', req.headers['stripe-signature'])
     // Get the signature sent by Stripe
     const signature = req.headers['stripe-signature'].toString();
+    console.log('signature', signature)
+
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
     event = stripe.webhooks.constructEvent(
       req.body,
@@ -24,20 +29,28 @@ export const initApp = (app, express) => {
       endpointSecret
     );
 
-
+    console.log('event', event)
+    console.log('event.type', event.type)
     // Handle the event
     if (event.type == 'checkout.session.completed') {
+      console.log('inside event completed')
       const checkout = event.data.object; //this object is similar to the checkout in the order controller
+      console.log('checkout', checkout)
 
       //once the event occur => clear cart of that user, update order status, update product stock
       const orderId = checkout.metadata.orderId;
+      console.log('orderId', orderId)
+
       const orderExist = await Order.findByIdAndUpdate(orderId, { status: orderStatus.SHIPPING }, { new: true });
-      await Cart.findOneAndUpdate({ user: orderExist.user }, { products: [] }, { new: true })
+      console.log("🚀 ~ app.post ~ orderExist:", orderExist)
+      const updatedCart = await Cart.findOneAndUpdate({ user: orderExist.user }, { products: [] }, { new: true })
+      console.log("🚀 ~ app.post ~ updatedCart:", updatedCart)
       for (const product of orderExist.products) {
-        await Product.findByIdAndUpdate(product.productId, { $inc: { stock: -product.quantity } }, { new: true })
+        const updatedProduct = await Product.findByIdAndUpdate(product.productId, { $inc: { stock: -product.quantity } }, { new: true })
+        console.log("🚀 ~ app.post ~ updatedProduct:", updatedProduct)
       }
     }
-
+    console.log('after condition')
     // Return a 200 res to acknowledge receipt of the event
     res.send();
   }));
